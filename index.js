fd42ff2b44dfdd49cd87e8f9da0c7d0069e20dda
Lawrence Yuen <lwrnceyuen@gmail.com>
2021-04-23 01:02:11 -0700

    create html template and stylesheet and functions for appending html code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { createDecipher } = require('crypto');
 
 //variable to store employee data
 const employees = [];
@@ -140,10 +141,10 @@ function newEmployee() {
     ]).then(function({name, role, id, email}) {
         let info = '';
         if(role === "Engineer") {
-            return info = 'Github username';
+          info = 'Github username';
         }
         if(role === "Intern") {
-            return info = 'school name';
+          info = 'school name';
         }
         inquirer.prompt([
             {
@@ -163,15 +164,110 @@ function newEmployee() {
                     if(role = 'Intern') {
                         newTeammate = new Intern(name, id, email, info);
                     }
-                    employees.push(newTeammate).then(function() {
+                    employees.push(newTeammate)
+                    newHTML(newTeammate).then(function() {
                         if(addMore === 'yes') {
                             newTeammate();
-                
+                        }
+                        if(addMore === 'no') {
+                          exitApp();
                         }
                     });
-                })
-    })
+                });
+    });
 };
 
+function generateHTML() {
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+      <link rel="stylesheet" href="./assets/css/styles.css">
+      <title>Team Profile</title>
+  </head>
+  <body>
+      <div class="container-fluid text-center">
+          <div class="row">
+              <div class="col">
+                  <div class="page-title">
+                      <h2>Your Team</h2>
+                  </div>
+              </div>
+          </div>
+          <div class="row">`;
+          fs.writeFile('./dist/team-output.html', html, function(err) {
+            if (err) {
+              console.log(err);
+            }
+          });
+};
+
+function newHTML(teammate) {
+  return new Promise(function(resolve, reject) {
+    const name = teammate.getName();
+    const role = teammate.getRole();
+    const id = teammate.getId();
+    const email = teammate.getEmail();
+    let data = '';
+
+    if (role === 'Manager') {
+      const office = teammate.getOffice();
+      return data = `
+      <div class="col-lg-4 col-sm-12">
+          <div class="card" style="width: 18rem;">
+              <img src="./assets/images/manager.jpg" class="card-img-top" alt="manager">
+              <div class="card-body">
+                  <h4 class="card-title">${name}</h5>
+                  <p class="card-text">Role: Manager</p>
+                  <p class="card-text">ID:${id}</p>
+                  <p class="card-text">Email:${email}</p>
+                  <p class="card-text">Office Number:${office}</p>
+              </div>
+          </div>
+      </div>`;
+    }
+    if (role === 'Engineer') {
+      const github = teammate.getGitHub();
+      return data = `
+      <div class="col-lg-4 col-sm-12">
+          <div class="card" style="width: 18rem;">
+              <img src="./assets/images/engineer.jpg" class="card-img-top" alt="engineer">
+              <div class="card-body">
+                  <h4 class="card-title">${name}</h5>
+                  <p class="card-text">Role: Engineer</p>
+                  <p class="card-text">ID:${id}</p>
+                  <p class="card-text">Email:${email}</p>
+                  <p class="card-text">GitHub:${github}</p>
+              </div>
+          </div>
+      </div>`;
+    }
+    if (role === 'Intern') {
+      const school = teammate.getSchool();
+      return data = `
+      <div class="col-lg-4 col-sm-12">
+          <div class="card" style="width: 18rem;">
+              <img src="./assets/images/intern.jpg" class="card-img-top" alt="intern">
+              <div class="card-body">
+                  <h4 class="card-title">${name}</h5>
+                  <p class="card-text">Role: Intern</p>
+                  <p class="card-text">ID:${id}</p>
+                  <p class="card-text">Email:${email}</p>
+                  <p class="card-text">School:${school}</p>
+              </div>
+          </div>
+      </div>`;
+    }
+    fs.appendFile('./dist/team-output.html', data, function(err) {
+      if(err) {
+        return reject(err);
+      };
+      return resolve();
+    })
+  }
+)};
 
 checkPrivileges();
